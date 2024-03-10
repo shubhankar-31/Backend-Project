201605d8b7af57230b6d529a2d73080f9bb58519
Shubhankar <shubhankar.sharma97@gmail.com>
2024-03-10 16:51:51 +0530

    made mongo DB atlas connection

---
+++ src/index.js
@@ -0,0 +1,46 @@
+//This syntax is fine but we can write a more improved version
+//require("dotenv").config({path:"./env"});
+import dotenv from "dotenv";
+dotenv.config({path:"./env"});
+
+import express from "express";
+
+const app=express();
+
+
+// Second Approach to connect a DB
+import connectDB from "./db/db.js";
+
+connectDB();
+
+
+
+
+
+// First Approach to connect to a DB using IIFE
+
+// sometimes dev's start and IIFE with a semi colon like 
+//;(async () => {})();
+// but we don't need it here 
+
+/*
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants";
+
+(async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{
+            console.error("ERROR",error);
+            throw error;
+        });
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on PORT ${process.env.PORT}`);
+        })
+    } 
+    catch (error){
+        console.error("ERROR",error);
+        throw error;
+    } 
+})();
+*/
