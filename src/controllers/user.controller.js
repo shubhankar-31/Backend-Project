f912cfb134b573642e939868a2533c1b28797037
Shubhankar <shubhankar.sharma97@gmail.com>
2024-04-06 08:57:06 +0530

    Bug Fixes + Comments

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,15 +24,17 @@ const registerUser=asyncHandler(async (req,res)=>{
 
     // here we are basically checking for empty fields
     // we could have also done it with multiple if conditions
-    if([fullName,email,username,password].some((field)=>field.trim()==="")){
+    if([fullName,email,username,password].some((field)=>field?.trim()==="")){
         throw new ApiError(400,"All fields are required ");
     }
 
-    const existUser=User.findOne({
+    //Step-->3
+    const existUser=await User.findOne({
         $or:[{username},{email}]
     });
     if(existUser) throw new ApiError(409,"User with email or username already exist");
 
+    //Step-->4
     const avatarLocalPath=req.files?.avatar[0]?.path;
     const coverImageLocalPath=req.files?.coverImage[0]?.path;
 
@@ -45,23 +47,30 @@ const registerUser=asyncHandler(async (req,res)=>{
     if(!avatar)
         throw new ApiError(400,"Avatar file is required");
 
-        const user=await User.create({
-            fullName,
-            avatar:avatar.url,
-            coverImage:coverImage?.url || "",
-            email,
-            password,
-            username:username.toLowerCase()
-        });
-        //User object without password and refresh token field
-       const userCreated=await User.findById(user._id).select(
-        "-password -refreshToken");
-        if(!userCreated)
-            throw new ApiError(500,"Something went Wrong while registering user");
-        
-        return res.status(201).json(
-            new ApiResponse(200,userCreated,"User Registered with success")
-        );
+    //Step->5
+    const user=await User.create({
+        //(USerModel object name , object name receiving through Req.body)
+        fullName,
+        avatar:avatar.url,
+        coverImage:coverImage?.url || "",
+        email,
+        password,
+        userName:username?.toLowerCase()
+    });
+
+    //Step->6
+    //User object without password and refresh token field
+    const userCreated=await User.findById(user._id).select(
+    "-password -refreshToken");
+
+    //Step->7
+    if(!userCreated)
+        throw new ApiError(500,"Something went Wrong while registering user");
+    
+    //Step->8
+    return res.status(201).json(
+        new ApiResponse(200,userCreated,"User Registered with success")
+    );
 
 }); 
 
