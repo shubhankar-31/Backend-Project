48fffa8b31751d0b396b3d923446e50503e4c47a
Shubhankar <shubhankar.sharma97@gmail.com>
2024-04-16 20:22:00 +0530

    Added User Account Managment Controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -158,11 +158,12 @@ const loginUser=asyncHandler (async (req,res)=>{
         },"User logged in Successfully")
     )
 
-        /* Inside the json response, we don't necesserily have to send AccessToke and
-        RefreshToken again because we have already done that with cookie.
-        However with this we are dealing cases like->
+        /* Inside the json response, we don't necesserily have to send AccessToken and
+        RefreshToken again because we have already set that in the
+        cookie("accessToken" and "refreshToken").
+        But with this we can deal with cases like->
         1. If user wants to store/save these token in local storage,
-        2. we can't set cookies in mobile development 
+        2. We can't set cookies during mobile development.
         
         */
 
@@ -233,9 +234,113 @@ const refreshAccessToken=asyncHandler(async (req,res)=>{
     
 });
 
+const changeCurrentUserPassword=asyncHandler(async (req,res)=>{
+    const{oldPass,newPass,confPass}=req.body;
+
+    if(newPass!==confPass)
+        throw new ApiError(400,"New Password and Conformation password did not match");
+    
+    const user=await User.findById(req.user?._id);
+
+    const passwordCorrect=await user.isPasswordCorrect(oldPass);
+    if(!passwordCorrect)
+        throw new ApiError(400,"Invalid Password");
+
+    user.password=newPass;
+    await user.save({validateBeforeSave:false});
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{},"Password was changed successfully"));
+});
+
+const getCurrentUser=asyncHandler(async (req,res)=>{
+    return res
+    .status(200)
+    .json(200,req.user,"current user fetched successfully")
+
+});
+
+const updateAccDetail=asyncHandler(async (req,res)=>{
+    const {fullName,email}=req.body;
+
+    if(!username && !email)
+        throw new ApiError(400,"All fields are required");
+
+        const user= await User.findByIdAndUpdate(
+            req.user?._id,
+            {
+                $set:{
+                    fullName:fullName,
+                    email:email
+                }
+            },
+            {new :true}
+            ).select("-password")
+
+    return res
+    .status(200)
+    .json(200,user,"Account deatils updated with success");
+});
+
+const updateUserAvatar=asyncHandler(async (req,res)=>{
+    const avatarLocalPath=req.file?.path;
+    if(!avatarLocalPath)
+        throw new ApiError(400,"Avatar File is missing");
+
+    const avatar=await uploadOnCloudinary(avatarLocalPath);
+    if(!avatar.url)
+        throw new ApiError(400,"Error while uploading avatar");
+
+    const user= await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar:avatar.url               
+            }
+        },
+        {new :true}
+        ).select("-password");
+
+    return res.status(200)
+    .json(new ApiResponse
+        (200,user,"Avatar updated Successfully"));
+
+});
+const updateUserCoverImage=asyncHandler(async (req,res)=>{
+    const coverImageLocalPath=req.file?.path;
+    if(!coverImageLocalPath)
+        throw new ApiError(400,"Cover Image File is missing");
+
+    const coverImage=await uploadOnCloudinary(coverImageLocalPath);
+    if(!coverImage.url)
+        throw new ApiError(400,"Error while uploading Cover Image");
+
+    const user= await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage:coverImage.url               
+            }
+        },
+        {new :true}
+        ).select("-password");
+
+        return res.status(200)
+        .json(new ApiResponse
+            (200,user,"Cover Image updated Successfully"));
+
+
+});
+
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentUserPassword,
+    getCurrentUser,
+    updateAccDetail,
+    updateUserAvatar,
+    updateUserCoverImage
 }
